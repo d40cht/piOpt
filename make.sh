76108f77777ba5da63dbea8cd40376c70012791d
Alex Wilson <alex_wilson@pobox.com>
2012-05-10 21:01:15 +0100

    Evolution

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-g++ -DNDEBUG -mcpu=arm1176jzf-s -mfpu=neon -mfloat-abi=softfp -O2 -I eigen invTest/test.cpp -o invTest/test
+g++ -DNDEBUG -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=softfp -O2 -I eigen invTest/test.cpp -o invTest/test
 #g++ -g -I eigen invTest/test.cpp -o invTest/test
